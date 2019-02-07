1a22dbeb7f68fa4d29f1e1b0a1d75ff8e7fb186d
Hanna Bennett <bennetthanna22@gmail.com>
2019-02-06 23:32:01 -0800

    Put each file name in button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import _ from 'lodash';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoaded: false,
+      selectedSong: null,
+      songs: null
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://3.88.74.36:3000')
+      .then(res => {
+        return res.json();
+      })
+      .then(jsonData => {
+        this.setState({ songs: jsonData, isLoaded: true });
+      })
+      .catch(err => {
+        alert(err);
+      });
+  }
+
+  renderList() {
+    const { songs } = this.state;
+    return (
+      <ul className="list-group list-group-flush">
+        {songs.map((s, index) => {
+          return (
+            <div key={index}>
+              <button type="button" className="list-group-item list-group-item-action">{songs[index].song}</button>
+            </div>
+          )
+        })}
+      </ul>
+    );
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div>
+          { this.state.isLoaded ? this.renderList() : null }
+        </div>
       </div>
     );
   }
