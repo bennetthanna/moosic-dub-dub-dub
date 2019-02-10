86e77e611673158c96efe966457ae9d02a487e47
Hanna Bennett <bennetthanna22@gmail.com>
2019-02-09 23:05:54 -0800

    Render audio source on select

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import _ from 'lodash';
 import 'bootstrap/dist/css/bootstrap.css';
-import { Table } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlay } from '@fortawesome/free-solid-svg-icons'
+import { Table, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
+import _ from 'lodash';
 
 class App extends Component {
   constructor(props) {
@@ -12,9 +12,12 @@ class App extends Component {
 
     this.state = {
       isLoaded: false,
-      selectedSong: null,
+      isSongSelected: false,
+      selectedSongIndex: null,
       songs: null
     }
+
+    this.selectSong = this.selectSong.bind(this);
   }
 
   componentDidMount() {
@@ -30,17 +33,23 @@ class App extends Component {
       });
   }
 
+  selectSong(event, index) {
+    event.preventDefault();
+    this.setState({ selectedSongIndex: index, isSongSelected: true });
+  }
+
   renderList() {
-    const { songs } = this.state;
+    const { songs, selectedSongIndex } = this.state;
+    const selectSong = this.selectSong;
     return (
       <tbody>
         {songs.map((s, index) => {
           return (
-            <tr>
+            <tr key={index}>
               <td>{songs[index].artist}</td>
               <td>{songs[index].album}</td>
               <td>{songs[index].song}</td>
-              <td><FontAwesomeIcon icon={faPlay} /></td>
+              <td><Button variant={selectedSongIndex === index ? "info" : "outline-info"} onClick={(event) => selectSong(event, index)}><FontAwesomeIcon icon={faPlay} /></Button></td>
             </tr>
           )
         })}
@@ -48,19 +57,41 @@ class App extends Component {
     );
   };
 
+  renderAudio() {
+    const { songs, selectedSongIndex } = this.state;
+    const selectedSong = songs[selectedSongIndex];
+    var songPath = _.get(selectedSong, 'artist') ? `${selectedSong.artist}/` : '';
+    songPath += _.get(selectedSong, 'album') ? `${selectedSong.album}/` : '';
+    songPath += _.get(selectedSong, 'song') ? selectedSong.song : '';
+    const s3Path = `https://s3.amazonaws.com/bucket-o-dub-dub-dub/${songPath}`;
+    return (
+      <div>
+        <audio controls>
+          <source src={s3Path} type="audio/mp4"></source>
+          Your browser does not support the audio element.
+        </audio>
+      </div>
+    );
+  }
+
   render() {
     return (
-      <Table striped bordered hover variant="dark">
-        <thead>
-          <tr>
-            <th>Artist</th>
-            <th>Album</th>
-            <th>Song</th>
-            <th></th>
-          </tr>
-        </thead>
-        { this.state.isLoaded ? this.renderList() : null }
-      </Table>
+      <div>
+      { this.state.isSongSelected ? this.renderAudio() : null }
+      <div>
+        <Table striped bordered hover variant="dark">
+          <thead>
+            <tr>
+              <th>Artist</th>
+              <th>Album</th>
+              <th>Song</th>
+              <th>Play That Funky Moosic</th>
+            </tr>
+          </thead>
+          { this.state.isLoaded ? this.renderList() : null }
+        </Table>
+      </div>
+      </div>
     );
   }
 }
